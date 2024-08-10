9b6dce7ba8ab856e14165f1908137ae53e6dad28
Lê Quốc Hải <lequochai26@gmail.com>
2024-08-10 21:24:16 +0700

    Added SQL Script for Day_6_1, CRUD_user.

---
+++ Files/CRUD_user.sql
@@ -0,0 +1,18 @@
+-- C (Create)
+-- INSERT INTO user(username, password, fullName, role)
+-- VALUES ('tranthib', 'BXinhGai1919', 'Trần Thị B', 'QC')
+
+-- U (Update)
+-- UPDATE user
+-- SET fullName='Nguyễn Văn Trí', role='QC'
+-- WHERE username='tranvana'
+
+-- D (Delete)
+-- DELETE FROM user
+-- WHERE username='tranthib'
+
+-- R (Read)
+-- SELECT * FROM user
+
+-- SELECT * FROM user
+-- WHERE username='tranvana'
